d16935e8860be6cb5618a936c6b9bc03cc3b6300
michael.kroes <michael.kroes@gmail.com>
2015-09-03 20:44:56 +0200

    First passing test for the substituion sypher

---
+++ Cypher.Tests/EncryptPlainText.feature
@@ -0,0 +1,46 @@
+﻿Feature: Encrypt Plain Text
+	
+	As a privacy nut I would like to encrypt text in the worst possible way by using a substitution cypher
+
+	The cypher will replace each letter with another letter. Each letter will be used exactly once.
+
+	My plain and text will be case insensitive and punctuation marks will not be encrypted
+
+Scenario: A simple plain text using all letters from the alphabet
+	Given the plain text
+	"""
+	the quick brown fox jumps over the lazy dog
+	"""
+	When I encrypt the text using the following letter substitution
+	| Plain text letter | Cypher text letter |
+	| A                 | B                  |
+	| B                 | C                  |
+	| C                 | D                  |
+	| D                 | E                  |
+	| E                 | F                  |
+	| F                 | G                  |
+	| G                 | H                  |
+	| H                 | I                  |
+	| I                 | J                  |
+	| J                 | K                  |
+	| K                 | L                  |
+	| L                 | M                  |
+	| M                 | N                  |
+	| N                 | O                  |
+	| O                 | P                  |
+	| P                 | Q                  |
+	| Q                 | R                  |
+	| R                 | S                  |
+	| S                 | T                  |
+	| T                 | U                  |
+	| U                 | V                  |
+	| V                 | W                  |
+	| W                 | X                  |
+	| X                 | Y                  |
+	| Y                 | Z                  |
+	| Z                 | A                  |
+	Then the cypher text should be
+	"""
+	UIF RVJDL CSPXO GPY KVNQT PWFS UIF MBAZ EPH
+	"""
+	
---
+++ Cypher.Tests/EncryptPlainTextSteps.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace Cypher.Tests
+{
+    [Binding]
+    public class EncryptPlainTextSteps
+    {
+        private string _plainText;
+        private string _encryptedText;
+
+        [Given(@"the plain text")]
+        public void GivenThePlainText(string plainText)
+        {
+            _plainText = plainText;
+        }
+
+        [When(@"I encrypt the text using the following letter substitution")]
+        public void WhenIEncryptTheTextUsingTheFollowingLetterSubstitution(Table table)
+        {
+            var cypher = new SubstitutionCypher();
+
+            foreach (var tableRow in table.Rows)
+            {
+                cypher.Substitute(tableRow[0]).For(tableRow[1]);
+            }
+
+            _encryptedText = cypher.Encrypt(_plainText);
+        }
+        
+        [Then(@"the cypher text should be")]
+        public void ThenTheCypherTextShouldBe(string expectedCypherText)
+        {
+            _encryptedText.Should().Be(expectedCypherText);
+        }
+    }
+
+    public class SubstitutionCypher
+    {
+        private readonly List<Substitution> _substitutionList;
+
+        public SubstitutionCypher()
+        {
+            _substitutionList = new List<Substitution>();
+        }
+
+        public Substitution Substitute(string letterToSubstitute)
+        {
+            var substitution = new Substitution(letterToSubstitute.ToUpper());
+
+            _substitutionList.Add(substitution);
+
+            return substitution;
+        }
+
+        public string Encrypt(string plainText)
+        {
+            var encryptedText = "";
+            foreach (var letter in plainText.ToUpper())
+            {
+                var substitution = _substitutionList.SingleOrDefault(
+                    s => s.LetterToSubstitute.Equals(letter.ToString(), StringComparison.OrdinalIgnoreCase));
+
+                if (substitution != null)
+                {
+                    encryptedText += substitution.LetterToSubstituteWith;
+                }
+                else
+                {
+                    encryptedText += letter;
+                }
+            }
+
+            return encryptedText;
+        }
+    }
+
+    public static class SubstitutionExtensionMethods
+    {
+        public static void For(this Substitution substitution, string letterToSubstituteWith)
+        {
+            substitution.LetterToSubstituteWith = letterToSubstituteWith.ToUpper();
+        }
+    }
+
+    public class Substitution
+    {
+        public string LetterToSubstitute { get; }
+        public string LetterToSubstituteWith { get; set; }
+
+        public Substitution(string letterToSubstitute)
+        {
+            LetterToSubstitute = letterToSubstitute;
+        }
+    }
+}
